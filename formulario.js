2ed3e54e06aa99611bc0eff7ac6de6357ed9fa01
Marisol Valverde <44270819+MarisolVal@users.noreply.github.com>
2020-10-13 03:29:51 -0500

    Add files via upload
    Entregables Métodos HTTP

---
+++ formulario.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const app = express();
+var listaF = new Array();
+var listaM = new Array();
+const hbs = require('hbs');
+// hbs.registerPartials(__dirname + '../public/view');
+app.set('views', 'public/view');
+app.set('view engine', 'hbs');
+
+
+var formFem = {
+    "Ninguna": 0,
+    "Barrer": 0,
+    "platos": 0,
+    "Cocinar": 0,
+    "Planchar": 0,
+    "super": 0,
+    "ropa": 0,
+};
+
+var formMas = {
+    "Ninguna": 0,
+    "Barrer": 0,
+    "platos": 0,
+    "Cocinar": 0,
+    "Planchar": 0,
+    "super": 0,
+    "ropa": 0,
+};
+
+
+app.get('/cargarAct', (req, res) => {
+    var actividad = req.query.actividad;
+    var genero = req.query.genero;
+    actividad = actividad.split(' ');
+    actividad = actividad[actividad.length - 1];
+
+    if (genero === 'femenino') {
+        formFem[actividad] += 1;
+
+    } else {
+        formMas[actividad] += 1;
+    }
+
+    res.render('resultados', { fm: formFem, fh: formMas });
+});
+
+
+app.post('/cargarAct', (req, res) => {
+    var actividad = req.body.actividad;
+    var genero = req.body.genero;
+    actividad = actividad.split(' ');
+    actividad = actividad[actividad.length - 1];
+    console.log(genero);
+    if (genero === 'femenino') {
+        formFem[actividad] += 1;
+    } else {
+        formMas[actividad] += 1;
+    }
+    res.render('resultados', { fm: formFem, fh: formMas });
+
+});
+
+app.put('/cargarAct/:genero', (req, res) => {
+    var genero = req.params.genero;
+    var actividad = req.body.actividad;
+    var cantidad = req.body.cantidad;
+    actividad = actividad.split(' ');
+    actividad = actividad[actividad.length - 1];
+    console.log(genero);
+    if (genero === 'femenino') {
+        formFem[actividad] = cantidad;
+        return res.json({
+            actividad,
+            femenino: formFem,
+            mensaje: `se actualizó el conteo de la actvidad a ${cantidad}`
+        });
+    } else {
+        formMas[actividad] = cantidad;
+        return res.json({
+            actividad,
+            masculino: formMas,
+            mensaje: `se actualizó el conteo de la actvidad a ${cantidad}`
+        });
+    }
+    res.render('resultados', { fm: formFem, fh: formMas });
+
+});
+app.delete('/cargarAct/:genero', (req, res) => {
+    var genero = req.params.genero;
+    var actividad = req.body.actividad;
+    if (genero === 'femenino') {
+        formFem[actividad] = 0;
+        return res.json({
+            actividad,
+            femenino: formFem,
+            mensaje: `se reinició el conteo la actividad ${actividad}`
+        });
+    } else {
+        formMas[actividad] = 0;
+        return res.json({
+            actividad,
+            masculino: formMas,
+            mensaje: `se reinició el conteo la actividad ${actividad}`
+        });
+    }
+
+
+});
+
+app.delete('/borrarTodo', (req, res) => {
+
+    for (const actividad in formFem) {
+        formFem[actividad] = 0;
+        formMas[actividad] = 0;
+    }
+
+    res.json({
+        mensaje: "Se reinició todo el conteo",
+        femenino: formFem,
+        masculino: formMas
+    }); 
+});
+
+module.exports = app;
